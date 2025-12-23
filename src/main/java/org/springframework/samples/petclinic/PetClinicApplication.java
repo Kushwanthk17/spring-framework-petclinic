62e9aa72d4d6e3fb50a4e887a22fb36b31844814
Kushwanth Kumar <madinenikushwanth40@gmail.com>
2025-12-23 22:58:09 +0530

    Enable JPA repositories in PetClinicApplication

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,14 +1,15 @@
+package org.springframework.samples.petclinic;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(
     basePackages = "org.springframework.samples.petclinic.repository.springdatajpa"
 )
-@EntityScan("org.springframework.samples.petclinic.model")
 public class PetClinicApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
