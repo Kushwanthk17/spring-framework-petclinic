3770aa431182582ec74ee1958a04b9e9c72a4053
Kushwanth Kumar <madinenikushwanth40@gmail.com>
2025-12-23 20:19:15 +0530

    Clarify main class name in ServletInitializer
    Updated comment to clarify main class name spelling.

--- src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
@@ -5,9 +5,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        // MAKE SURE THIS CLASS NAME MATCHES YOUR MAIN JAVA FILE EXACTLY
-        return application.sources(PetClinicApplication.class); 
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// CHECK SPELLING: Is your main file PetClinicApplication or PetclinicApplication?
+		// Use the EXACT name of the file in your folder.
+		return application.sources(PetClinicApplication.class);
+	}
+
 }
