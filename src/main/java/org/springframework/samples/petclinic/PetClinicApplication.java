e9e5c42655a77c2119fd575e0b4a7b8bb0d65bd4
Kushwanth Kumar <madinenikushwanth40@gmail.com>
2025-12-23 22:52:02 +0530

    Modify PetClinicApplication to exclude DataSourceAutoConfiguration
    Exclude DataSourceAutoConfiguration from Spring Boot application.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,13 +1,16 @@
-package org.springframework.samples.petclinic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class
+    }
+)
 public class PetClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
 }
