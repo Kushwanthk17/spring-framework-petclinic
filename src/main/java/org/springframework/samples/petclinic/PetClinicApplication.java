236d12a8b5ab1a35d1ef79e3729ac99a4e787dbd
Kushwanth Kumar <madinenikushwanth40@gmail.com>
2025-12-23 22:54:33 +0530

    Refactor PetClinicApplication configuration
    Removed exclusion of DataSourceAutoConfiguration and added annotations for JPA repositories and entity scanning.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -3,13 +3,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {
-        DataSourceAutoConfiguration.class
-    }
+@SpringBootApplication
+@EnableJpaRepositories(
+    basePackages = "org.springframework.samples.petclinic.repository.springdatajpa"
 )
+@EntityScan("org.springframework.samples.petclinic.model")
 public class PetClinicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
